ff9484096062cef3cd1ce8257503f52d1c1de9a6
root <root@Kali>
2025-01-22 21:57:09 +0700

    22-01-2025

---
+++ accounts/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+
+class CustomSigninForm(AuthenticationForm):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields["username"].widget.attrs.update({"class":"form-control","max-length":30,"required":True})
+		self.fields["password"].widget.attrs.update({"class":"form-control","max-length":30,"required":True})
+
+
+class CustomSignupForm(forms.ModelForm):
+	username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput(attrs={"class":"form-control"}))
+	email = forms.EmailField(label="Email", max_length=100, widget=forms.EmailInput(attrs={"class":"form-control"}))
+	password1 = forms.CharField(label="Password" ,max_length=30, widget=forms.PasswordInput(attrs={"class":"form-control"}))
+	password2 = forms.CharField(label="Konfirm Password", max_length=30, widget=forms.PasswordInput(attrs={"class":"form-control"}))
+
+	class Meta:
+		model = User
+		fields = ["username","email","password1","password2"]
+
+
---
+++ accounts/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import CustomSigninForm, CustomSignupForm
+from datetime import datetime
+
+def index(request):
+	return render(request, 'index.html')
+
+def signin(request):
+	context = {
+		"title" : "SignIn Form"
+	}
+	if request.method == "POST":
+		form = CustomSigninForm(data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return HttpResponseRedirect(reverse('index'))
+		else:
+			messages.error(request, "SignIn Failed, Pastikan lagi Inputannya.")
+	else:
+		form = CustomSigninForm()
+	context['form'] = form
+	return render(request, 'signin.html', context)
+
+def signup(request):
+	form = CustomSignupForm()
+	context = {
+		"title" : "SignUp Form",
+	}
+	if request.method == "POST":
+		form = CustomSignupForm(data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get("username")
+			email = form.cleaned_data.get("email")
+			password1 = form.cleaned_data.get("password1")
+			password2 = form.cleaned_data.get("password2")
+			today = datetime.today().date()
+			if len(password1) >= 8 and len(password2) >= 8:
+				if password1 == password2:
+					user = User(username=username, email=email, date_joined=today)
+					user.password = make_password(password2)
+					user.save()
+					messages.success(request, "Pendaftaran Berhasil, Silakan Coba Masuk.")
+					return HttpResponseRedirect(reverse('index'))
+				else:
+					message.error(request, "Pendaftaran Gagal", "Password Tidak Sama dengan Konfirmasi Password.")
+			else:
+				messages.error(request, "Pendaftaran Gagal, Panjang Password minimal 8 Karakter.")
+		else:
+			messages.error(request, "Pendaftaran Gagal, Pastikan lagi Inputannya.")
+
+	return render(request, 'signup.html', {'form':form, 'context':context})
+
